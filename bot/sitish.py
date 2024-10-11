cc9399b31735f73e6bc177289ce035ea0fdb2660
Nikos <60401202+tenmakenzou@users.noreply.github.com>
2024-10-11 13:28:43 +0300

    Update sitish.py

--- bot/sitish.py
+++ bot/sitish.py
@@ -110,7 +110,7 @@ def run():
                                     if count == 0:
                                         
                                         result += "\n***Πρωινό***\n"  
-                                        result += '-Γάλα φρέσκο ζεστό ή κρύο, Τσάι σε διάφορες γεύσεις, μαρμελάδες σε διάφορες γεύσεις, Μαργαρίνη, Μέλι, Φρυγανιές σίτου, Ψωμί, Κέικ-Fresh milk hot or cold, Tea in various flavors, jams in various flavors, Margarine, Honey, Wheat toast, Bread, Cake\n'
+                                        result += '- Γάλα φρέσκο ζεστό ή κρύο, Τσάι σε διάφορες γεύσεις, μαρμελάδες σε διάφορες γεύσεις, Μαργαρίνη, Μέλι, Φρυγανιές σίτου, Ψωμί, Κέικ-Fresh milk hot or cold, Tea in various flavors, jams in various flavors, Margarine, Honey, Wheat toast, Bread, Cake\n'
                                         result += "\n***Μεσημεριανό***\n"  
                                     
                                     if count == 3:
