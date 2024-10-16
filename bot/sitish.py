c3c3703f3ddfa9fb6fd87353b0d6fe74090d58fc
Nikos <60401202+tenmakenzou@users.noreply.github.com>
2024-10-16 15:15:30 +0300

    Optimization

--- bot/sitish.py
+++ bot/sitish.py
@@ -4,77 +4,10 @@ from datetime import datetime
 
 def run():
 
-
-    week_1 = [
-        'C3', 'D3', 'E3', 
-        'F3', 'G3', 'H3', 
-        'I3',
-        'C4', 'D4', 'E4', 
-        'F4', 'G4', 'H4', 
-        'I4',
-        'C5', 'D5', 'E5', 
-        'F5', 'G5', 'H5', 
-        'I5',
-        'C6', 'D6', 'E6', 
-        'F6', 'G6', 'H6', 
-        'I6'
-    ]
-
-    week_2 = [
-        'C26', 'D26', 'E26', 
-        'F26', 'G26', 'H26', 
-        'I26',
-        'C27', 'D27', 'E27', 
-        'F27', 'G27', 'H27', 
-        'I27',
-        'C28', 'D28', 'E28', 
-        'F28', 'G28', 'H28', 
-        'I28',
-        'C29', 'D29', 'E29', 
-        'F29', 'G29', 'H29', 
-        'I29'
-    ]
-
-    week_3 = [
-        'C48', 'D48', 'E48', 
-        'F48', 'G48', 'H48', 
-        'I48',
-        'C49', 'D49', 'E49', 
-        'F49', 'G49', 'H49', 
-        'I49',
-        'C50', 'D50', 'E50', 
-        'F50', 'G50', 'H50', 
-        'I50',
-        'C51', 'D51', 'E51', 
-        'F51', 'G51', 'H51', 
-        'I51'
-    ]
-
-    week_4 = [
-        'C71', 'D71', 'E71', 
-        'F71', 'G71', 'H71', 
-        'I71',
-        'C72', 'D72', 'E72', 
-        'F72', 'G72', 'H72', 
-        'I72',
-        'C73', 'D73', 'E73', 
-        'F73', 'G73', 'H73', 
-        'I73',
-        'C74', 'D74', 'E74', 
-        'F74', 'G74', 'H74', 
-        'I74',
-        'C75', 'D75', 'E75', 
-        'F75', 'G75', 'H75', 
-        'I75'
-    ]
-
-
-
     wb = openpyxl.load_workbook('menu.xlsx')
     sheet = wb['Φύλλο1'] 
 
     target_date = datetime.now()  # datetime of the current day
-
     def get_row():
         for row in sheet.iter_rows():
             for cell in row:
@@ -84,28 +17,16 @@ def run():
                         return cell.coordinate
 
     x = get_row()
-
-    y = 0 
-
-    if x in week_1:
-        y = 2
-    if x in week_2:
-        y = 26
-    if x in week_3:
-        y = 46
-    if x in week_4:
-        y = 71
+    
+    lista = ['Δευτέρα-Monday','Τρίτη-Tuesday','Τετάρτη-Wednesday','Πέμπτη-Thursday','Παρασκευή-Friday','Σάββατο-Saturday','Κυριακή-Sunday',None," ",""] # list of objects to avoid printing
 
 
-    lista = ['Δευτέρα-Monday','Τρίτη-Tuesday','Τετάρτη-Wednesday','Πέμπτη-Thursday','Παρασκευή-Friday','Σάββατο-Saturday','Κυριακή-Sunday']
-
-
-    def get_date(x, y):
+    def get_date(x):
         count = 0
         result = ""  
         
-        for cell in sheet[x[0]][y:]:
-            if cell.value not in (None, " ", "") and not isinstance(cell.value, datetime) and cell.value not in lista:
+        for cell in sheet[x[0]][int(x[1]+x[2]):]: #column[x[0]] and column number x[1],[2]
+            if not isinstance(cell.value, datetime) and cell.value not in lista:
                if count == 0:
                                         
                     result += "\n***Πρωινό***\n"  
@@ -122,5 +43,5 @@ def run():
         
         return result  
 
-    help = get_date(x,y)
-    return help
+
+    return get_date(x)
