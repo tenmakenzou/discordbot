8dddb9de80e118002e040e4e968d143952951894
Nikos <60401202+tenmakenzou@users.noreply.github.com>
2026-01-20 01:16:45 +0200

    Added departments

--- bot/botmain.py
+++ bot/botmain.py
@@ -105,6 +105,92 @@ async def on_ready():
         print(f"Failed to sync commands: {e}")
 
 
+
+@tree.command(name="departments", description="Show all UNIWA departments")
+async def departments(interaction: discord.Interaction):
+
+    embed = discord.Embed(
+        title="Τμήματα Πανεπιστημίου Δυτικής Αττικής",
+        colour=0x000000
+    )
+
+    embed.add_field(
+        name="Δημόσια Υγεία",
+        value=(
+            "[Τμήμα Δημόσιας και Κοινοτικής Υγείας](https://pch.uniwa.gr/)\n"
+            "[Τμήμα Πολιτικών Δημόσιας Υγείας](http://php.uniwa.gr)\n"
+        ),
+        inline=False
+    )
+
+    embed.add_field(
+        name="Διοικητικές, Οικονομικές & Κοινωνικές Επιστήμες",
+        value=(
+            "[Τμήμα Αγωγής και Φροντίδας στην Πρώιμη Παιδική Ηλικία](http://ecec.uniwa.gr/)\n"
+            "[Τμήμα Αρχειονομίας, Βιβλιοθηκονομίας και Συστημάτων Πληροφόρησης](http://alis.uniwa.gr)\n"
+            "[Τμήμα Διοίκησης Επιχειρήσεων](http://www.ba.uniwa.gr/)\n"
+            "[Τμήμα Διοίκησης Τουρισμού](http://tourism.uniwa.gr/)\n"
+            "[Τμήμα Κοινωνικής Εργασίας](http://sw.uniwa.gr/)\n"
+            "[Τμήμα Λογιστικής και Χρηματοοικονομικής](https://accfin.uniwa.gr/)"
+        ),
+        inline=False
+    )
+
+    embed.add_field(
+        name="Επιστήμες Τροφίμων",
+        value=(
+            "[Τμήμα Επιστήμης και Τεχνολογίας Τροφίμων](http://fst.uniwa.gr/)\n"
+            "[Τμήμα Επιστημών Οίνου, Αμπέλου και Ποτών](http://wvbs.uniwa.gr/)"
+        ),
+        inline=False
+    )
+
+    embed.add_field(
+        name="Επιστήμες Υγείας & Πρόνοιας",
+        value=(
+            "[Τμήμα Βιοϊατρικών Επιστημών](http://bisc.uniwa.gr/)\n"
+            "[Τμήμα Εργοθεραπείας](http://ot.uniwa.gr/)\n"
+            "[Τμήμα Μαιευτικής](http://midw.uniwa.gr/)\n"
+            "[Τμήμα Νοσηλευτικής](http://nurs.uniwa.gr/)\n"
+            "[Τμήμα Φυσικοθεραπείας](http://www.phys.uniwa.gr/)"
+        ),
+        inline=False
+    )
+
+    embed.add_field(
+        name="Εφαρμοσμένες Τέχνες & Πολιτισμός",
+        value=(
+            "[Τμήμα Γραφιστικής και Οπτικής Επικοινωνίας](http://www.gd.uniwa.gr)\n"
+            "[Τμήμα Εσωτερικής Αρχιτεκτονικής](http://ia.uniwa.gr)\n"
+            "[Τμήμα Συντήρησης Αρχαιοτήτων και Έργων Τέχνης](http://cons.uniwa.gr)\n"
+            "[Τμήμα Φωτογραφίας και Οπτικοακουστικών Τεχνών](http://phaa.uniwa.gr)"
+        ),
+        inline=False
+    )
+
+    embed.add_field(
+        name="Μηχανικοί & Πολυτεχνική",
+        value=(
+            "[Τμήμα Μηχανικών Βιοϊατρικής](http://bme.uniwa.gr)\n"
+            "[Τμήμα Ηλεκτρολόγων και Ηλεκτρονικών Μηχανικών](http://eee.uniwa.gr)\n"
+            "[Τμήμα Μηχανικών Βιομηχανικής Σχεδίασης και Παραγωγής](http://idpe.uniwa.gr)\n"
+            "[Τμήμα Μηχανικών Πληροφορικής και Υπολογιστών](http://www.ice.uniwa.gr)\n"
+            "[Τμήμα Μηχανικών Τοπογραφίας και Γεωπληροφορικής](http://www.geo.uniwa.gr)\n"
+            "[Τμήμα Μηχανολόγων Μηχανικών](http://mech.uniwa.gr)\n"
+            "[Τμήμα Ναυπηγών Μηχανικών](http://www.na.uniwa.gr)\n"
+            "[Τμήμα Πολιτικών Μηχανικών](http://civ.uniwa.gr)"
+        ),
+        inline=False
+    )
+
+    embed.set_footer(
+        text=f"Requested by {interaction.user}",
+        icon_url=interaction.user.display_avatar.url
+    )
+
+    await interaction.response.send_message(embed=embed)
+
+
 @tree.command(name="s", description="Get today's or tomorrow's schedule")
 @app_commands.describe(arg="Choose 'today' or 'tomorrow'")
 async def s(interaction: discord.Interaction, arg: str):
@@ -132,6 +218,7 @@ async def help_command(interaction: discord.Interaction):
     embed.add_field(name="/s tomorrow", value="Tomorrow's schedule", inline=False)
     embed.add_field(name="/f", value="Shows if library/restaurant is open", inline=False)
     embed.add_field(name="/services", value="Shows university services", inline=False)
+    embed.add_field(name="/departments", value="Shows university's departments sites", inline=False)
     embed.add_field(
         name="Github",
         value="https://github.com/tenmakenzou/discordbot/",
